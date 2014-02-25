8dc46f70b3e91ecd57ea294d16003c0d36547b90
David Meier <david.meier127@gmail.com>
2014-02-24 23:02:02 -0600

    makes echo and set environment variables functional

--- main.c
+++ main.c
@@ -4,53 +4,101 @@
 #include <dirent.h>
 #include <unistd.h>
 
-/*
-Function that listens for input.
-*/
-void inputloop() {
-    char line[256];
-    gets(line);
+void cd(char *nextData) {
+
+}
+
+void clr() {
+
+}
+
+void dir(char nextData) {
     DIR *dir;
     struct dirent *file;
-    char * nextData;
-      nextData = strtok(line," ");
-      while (nextData != NULL)
-      {
-        printf("%s\n",nextData);
-        if (strcmp("cd", nextData) == 0){
-            printf("cd\n");
-        }else if (strcmp("clr", nextData) == 0) {
-            printf("clr\n");
-        }else if (strcmp("dir", nextData) == 0) {
-            nextData = strtok(NULL, " ");
-            if (nextData == NULL){
-                nextData = "/";
-            }
-            if ((dir = opendir(("%s", nextData))) == NULL)
-                perror("opendir() error");
-            while((file = readdir(dir)) != NULL)
-                printf("  %s\t", file->d_name);
-            printf("\n");
-        }else if (strcmp("environ", nextData) == 0) {
+    nextData = strtok(NULL, " ");
+    if (nextData == NULL){
+        nextData = "/";
+    }
+    if ((dir = opendir(("%s", nextData))) == NULL)
+        perror("opendir() error");
+    while((file = readdir(dir)) != NULL)
+        printf("  %s\t", file->d_name);
+    printf("\n");
+}
 
-        }else if (strcmp("help", nextData) == 0) {
+void environ() {
 
-        }else if (strcmp("pause", nextData) == 0) {
+}
 
-        }else if (strcmp("quit", nextData) == 0) {
-            printf("yep");
-            exit(EXIT_SUCCESS);
-        }else{printf("not a command\n");}
+void echo(char *nextData) {
+    if (nextData == NULL){
+        printf("please enter a comment");
+    }else{
         nextData = strtok(NULL, " ");
-      }
+        while(nextData !=NULL){
+            printf("%s ", nextData);
+            nextData = strtok(NULL, " ");
+        }
+        printf("\n");
+    }
+}
+
+void help() {
+
+}
+
+void mypause() {
+
 }
 
 /* function to set environment values and set starting location.
 
 */
 void setVariables() {
-
+    char cwd[1024];
+    char *pathname;
+    char *name= "shell";
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        pathname = malloc(strlen(name)+strlen(cwd)+1);
+        sprintf(pathname, "%s=%s", name, cwd);
+        putenv(pathname);
+    }
+    else
+        perror("getcwd() error");
+    return 0;
 }
+/*
+Function that listens for input.
+*/
+void inputloop() {
+
+    char line[256];
+    gets(line);
+    char * nextData;
+      nextData = strtok(line," ");
+      while (nextData != NULL)
+      {
+        if (strcmp("cd", nextData) == 0){
+            cd(nextData);
+        }else if (strcmp("clr", nextData) == 0) {
+            printf("clr\n");
+        }else if (strcmp("dir", nextData) == 0) {
+            dir(nextData);
+        }else if (strcmp("environ", nextData) == 0) {
+
+        }else if (strcmp("echo", nextData) == 0) {
+            echo(nextData);
+        }else if (strcmp("help", nextData) == 0) {
+
+        }else if (strcmp("pause", nextData) == 0) {
+
+        }else if (strcmp("quit", nextData) == 0) {
+            exit(EXIT_SUCCESS);
+        }else{printf("not a command\n");}
+        nextData = strtok(NULL, " ");
+      }
+}
+
 
 int main(void)
 {
