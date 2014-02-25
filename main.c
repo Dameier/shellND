18e1afb8288e0b817aab9bdae24b27c306b71952
David Meier <david.meier127@gmail.com>
2014-02-24 20:56:16 -0600

    added code to show files at a given location

--- main.c
+++ main.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <unistd.h>
 
+/*
+Function that listens for input.
+*/
 void inputloop() {
     char line[256];
     gets(line);
+    DIR *dir;
+    struct dirent *file;
     char * nextData;
-      printf ("Splitting string \"%s\" into tokens:\n",line);
       nextData = strtok(line," ");
       while (nextData != NULL)
       {
         printf("%s\n",nextData);
         if (strcmp("cd", nextData) == 0){
-            printf("cd");
+            printf("cd\n");
         }else if (strcmp("clr", nextData) == 0) {
-            printf("clr");
+            printf("clr\n");
         }else if (strcmp("dir", nextData) == 0) {
-
+            nextData = strtok(NULL, " ");
+            if (nextData == NULL){
+                nextData = "/";
+            }
+            if ((dir = opendir(("%s", nextData))) == NULL)
+                perror("opendir() error");
+            while((file = readdir(dir)) != NULL)
+                printf("  %s\t", file->d_name);
+            printf("\n");
         }else if (strcmp("environ", nextData) == 0) {
 
         }else if (strcmp("help", nextData) == 0) {
@@ -26,17 +40,23 @@ void inputloop() {
         }else if (strcmp("quit", nextData) == 0) {
             printf("yep");
             exit(EXIT_SUCCESS);
-        }else{printf("not a command");}
+        }else{printf("not a command\n");}
         nextData = strtok(NULL, " ");
       }
 }
 
+/* function to set environment values and set starting location.
+
+*/
+void setVariables() {
+
+}
+
 int main(void)
 {
-    printf("Hello World!\n");
+    setVariables();
     while (1) {
         inputloop();
     }
     return 0;
 }
-
